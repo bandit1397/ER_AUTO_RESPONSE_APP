52bbe3db7a6b7da5e44e7e28792f6d5e03ee7748
bandit <80446239+bandit1397@users.noreply.github.com>
2026-04-27 13:53:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -288,4 +288,4 @@ if __name__ == "__main__":
 
     port = int(os.environ.get("PORT", 5000))
 
-    socketio.run(app, host="0.0.0.0", port=port)
+    socketio = SocketIO(app, cors_allowed_origins="*", async_mode="eventlet")
